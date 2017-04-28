84f2ec69b3ab487d2f0dece21bf62914a6bfab5b
lieon <lieoncx@gmail.com>
2017-04-29 00:44:59 +0800

    depoly

---
+++ server/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LOG_FILE="/var/log/blog_deploy.log"
+
+date >> "$LOG_FILE"
+echo "Start deployment" >>"$LOG_FILE"
+cd /Path/need/be/deployed/
+echo "pulling source code..." >> "$LOG_FILE"
+git checkout origin gh-pages
+git pull origin gh-pages
+echo "Finished." >>"$LOG_FILE"
+echo >> $LOG_FILE
